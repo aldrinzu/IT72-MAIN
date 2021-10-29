1367a70a9700c3c1fdc11724bc5c175accc287f1
aldrin <aldrin.01udiong@gmail.com>
2021-10-29 19:43:18 +0800

    it72

---
+++ Midterms/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = -5.0f;
+    private float gravityValue = -9.81f;
+
+    void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+        //
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit");
+    }
+}
